592d1b90d7330cc15ac12b066a4922e6614aa00f
Bitumnus <dariia.zamrachilo@gmail.com>
2020-05-16 10:04:04 +0300

    Updates

---
+++ precache-manifest.17744ed4f79367f04120ed66c6d94367.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eca3e43774d035252127ed3d2a6d1225",
+    "url": "/fluffy-popup/index.html"
+  },
+  {
+    "revision": "e8225a7b10925d21f217",
+    "url": "/fluffy-popup/static/css/main.aba7f1ca.chunk.css"
+  },
+  {
+    "revision": "c63681701e3647b4f404",
+    "url": "/fluffy-popup/static/js/2.8cdfa74c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fluffy-popup/static/js/2.8cdfa74c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8225a7b10925d21f217",
+    "url": "/fluffy-popup/static/js/main.22a04d75.chunk.js"
+  },
+  {
+    "revision": "6a1a6c6c3db213727d38",
+    "url": "/fluffy-popup/static/js/runtime-main.6195e3ab.js"
+  }
+]);
